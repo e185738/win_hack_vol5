ef3edb73744db59ee9b3aca50c1000c61415aaac
Rei Yamashiro <e185738@ie.u-ryukyu.ac.jp>
2020-12-27 00:48:01 +0900

    create send html

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
-from flask import Flask
-from flask import render_template
- 
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
- 
+
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+@app.route('/send', methods=["post"])
+def post():
+    ms = request.form["message"]
+    print(ms)
+    return render_template('send.html', ms=ms)
